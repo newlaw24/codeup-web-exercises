ca42c31b43c1f7f669142e7c0fba9a8ea2dfaa0f
Ramiro <ramiro.delossantos101@gmail.com>
2017-10-11 17:21:31 -0500

    added external_js and inline_js.html files and external_js

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external js");
+
+alert ("welcome to my website");
+
+var color = prompt("what is your favorite color?");
+alert ("great, + color + is my favorite color too")
